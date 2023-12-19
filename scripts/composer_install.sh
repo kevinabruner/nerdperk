5bff32382a0bb7559d9736633c148118285508ee
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2023-12-19 18:47:08 -0500

    moves composer files regardless of extension

--- scripts/composer_install.sh
+++ scripts/composer_install.sh
@@ -2,7 +2,7 @@
 source /etc/environment
 
 ###installing composer###
-cp $gitDir/composer.json $gitDir/composer.lock /var/www/
+cp $gitDir/composer.* /var/www/
 rm -R /var/www/*
 cd /var/www/
 sudo -u $linuxUser -g www-data composer install
