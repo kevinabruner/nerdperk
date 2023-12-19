c54e4d55283827f11275ad0e19be910e6fc48c5f
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2023-12-19 13:13:11 -0500

    removing yes pipe

--- scripts/composer_install.sh
+++ scripts/composer_install.sh
@@ -5,4 +5,4 @@ source /etc/environment
 cp $gitDir/composer.json $gitDir/composer.lock /var/www/
 rm -R /var/www/web /var/www/html
 cd /var/www/
-yes | sudo -u $linuxUser -g www-data composer install
+sudo -u $linuxUser -g www-data composer install
