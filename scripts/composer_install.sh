edc1161eb98e189d62cc65c27a552d7a25fbebf4
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2024-03-13 00:34:31 -0400

    don't wipe /var/www

--- scripts/composer_install.sh
+++ scripts/composer_install.sh
@@ -2,7 +2,7 @@
 source /etc/environment
 
 ###installing composer###
-rm -R /var/www/*
+rm -R /var/www/vendor 
 cp $gitDir/composer.* /var/www/
 cd /var/www/
 sudo -u $linuxUser -g www-data composer install
