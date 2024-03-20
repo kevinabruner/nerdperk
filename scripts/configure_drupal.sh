a6718c146d2d883076c5e9fa8e8bbd178a22e4c7
Kevin Bruner-Prime <kevin.a.bruner@gmail.com>
2024-03-20 08:40:34 -0400

    using ceph first attempt

--- scripts/configure_drupal.sh
+++ scripts/configure_drupal.sh
@@ -7,11 +7,10 @@ cd $settingsDir
 cp $gitDir/settings.php $settingsDir/
 
 # Replace values in settings.php
-
-sudo sed -i.bak -E "s/\@@@dbName/$dbName/g" settings.php
-sudo sed -i.bak -E "s/\@@@username/$username/g" settings.php
-sudo sed -i.bak -E "s/\@@@password/$password/g" settings.php
-sudo sed -i.bak -E "s/\@@@dbHost/$dbHost/g" settings.php
+sed -i.bak -E "s/\@@@dbName/$dbName/g" settings.php
+sed -i.bak -E "s/\@@@username/$username/g" settings.php
+sed -i.bak -E "s/\@@@password/$password/g" settings.php
+sed -i.bak -E "s/\@@@dbHost/$dbHost/g" settings.php
 
 # Remove backup files created by sed
 rm settings.php.bak
@@ -25,5 +24,11 @@ sudo chown -R $linuxUser:www-data /var/www
 #updates using drush
 cd /var/www
 drush='/var/www/vendor/drush/drush/drush'
-$drush cr
-sudo -u $linuxUser -g www-data $drush updb
+
+$drush cr -vvv
+sudo -u $linuxUser -g www-data $drush updb -vvv
+
+#set perms on ceph files
+cephFiles="/var/www/web/sites/default/files"
+sudo chown -R $linuxUser:www-data $cephFiles
+sudo chmod 775 -R $linuxUser:www-data $cephFiles
